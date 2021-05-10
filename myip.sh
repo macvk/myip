cafd6249a461675261a25cc1ee3e898e687bc684
vadim <tutumbul@gmail.com>
2021-05-10 09:42:32 +0300

    minor changes

--- myip.sh
+++ myip.sh
@@ -9,38 +9,36 @@ function echo_bold {
     echo -e "${BOLD}${1}${NC}"
 }
 
-function program_exit {
+function check_program_exist {
     command -v $1 > /dev/null
     if [ $? -ne 0 ]; then
-        echo_error "Please, install \"$1\""
-        exit $error_code
+        echo "Please, install \"$1\""
+        exit 1
     fi
-    increment_error_code
 }
 
 function check_internet_connection {
     curl --silent --head  --request GET "https://${api_domain}" | grep "200 OK" > /dev/null
     if [ $? -ne 0 ]; then
-        echo_error "No internet connection."
-        exit $error_code
+        echo "No internet connection."
+        exit 1
     fi
-    increment_error_code
 }
 
-program_exit curl
+check_program_exist curl
 check_internet_connection
 
 ipv4=$(curl --silent "https://ipv4.${api_domain}/")
 ipv6=$(curl --silent "https://ipv6.${api_domain}/")
 
 if [ ! -z "$ipv4" ]; then
-	echo_bold "Your IPv4:"
-	echo $ipv4
+        echo_bold "Your IPv4:"
+        echo $ipv4
 fi
 
 if [ ! -z "$ipv6" ]; then
-	echo_bold "Your IPv6:"
-	echo $ipv6
+        echo_bold "Your IPv6:"
+        echo $ipv6
 fi
 
 exit 0
